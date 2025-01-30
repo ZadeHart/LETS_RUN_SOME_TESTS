1271f87431eb167a6d83a44e9170aaf9101cb110
Zade Hart <zadehartpro@gmail.com>
2025-01-29 19:59:47 -0700

    Added Component test.

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -17,32 +17,22 @@ describe('Quiz Component', () => {
     cy.mount(<Quiz />);
     cy.get('button').contains('Start Quiz').click();
     cy.get('.card').should('be.visible');
-    // cy.get('h2').should('not.be.empty');
+    cy.get('h2').should('not.be.empty');
   });
 
   it('should answer questions and complete the quiz', () => {
     cy.mount(<Quiz />);
     cy.get('button').contains('Start Quiz').click();
-
-    // Answer questions
     cy.get('button').contains('1').click();
-
-    // Verify the quiz completion
     cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
   });
 
   it('should restart the quiz after completion', () => {
     cy.mount(<Quiz />);
     cy.get('button').contains('Start Quiz').click();
-
-    // Answer questions
     cy.get('button').contains('1').click();
-
-    // Restart the quiz
     cy.get('button').contains('Take New Quiz').click();
-
-    // Verify the quiz is restarted
     cy.get('.card').should('be.visible');
-    // cy.get('h2').should('not.be.empty');
+    cy.get('h2').should('not.be.empty');
   });
 });
